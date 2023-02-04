1fe1be019bf2074f71604dd6ac6016fda2c9ce0e
Promiseejiro <promiseejiro43@gmail.com>
2023-02-03 23:58:39 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Main from "./main";
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+export default App;
